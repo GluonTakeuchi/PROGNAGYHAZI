49b269fb1cd8186f4de288e4a651c492fe6967da
GluonTakeuchi <gluon.takeuchi@gmail.com>
2015-10-31 17:02:14 +0100

    Kiidulás

---
+++ Nagyhazi/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("fahfsajlfhasjfhaslfhalfhalsfhalfhslafhakhfsakfhalfhaksfhalfhahhlhlhlhlhkyxyxyxyxyyxyyyyyxyxyxcx\n");
+    return 0;
+}
